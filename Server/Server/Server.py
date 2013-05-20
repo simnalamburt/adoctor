67bf253b118510ffe8ef75ec5ca63ac79fc76393
Hyeon Kim <uie10@naver.com>
2013-05-21 04:04:51 +0900

    주석추가, 인코딩 변경

--- Server/Server/Server.py
+++ Server/Server/Server.py
@@ -1,4 +1,4 @@
-# coding=cp949
+﻿# coding=utf-8
 
 from socket import *
 from thread import start_new_thread
@@ -11,6 +11,7 @@ recvlen = 1024
 def response(key):
     return key
 
+# 사용자 연결 핸들러 정의
 def handler(clientsock, addr):
     while 1:
         data = clientsock.recv(recvlen)
@@ -22,6 +23,10 @@ def handler(clientsock, addr):
     clientsock.close()
     print repr(addr), '- closed connection'
 
+# Main 함수
+# 리스너 소켓을 등록하여, 사용자 연결을 대기한다
+# 사용자 연결이 들어올경우 해당 사용자에 대한 스레드를 하나 생성하고,
+# 다시 다음 사용자 입력을 기다린다
 if __name__ == '__main__':
     serversock = socket(AF_INET, SOCK_STREAM)
     serversock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
