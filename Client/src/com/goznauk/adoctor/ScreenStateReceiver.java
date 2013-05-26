6a0fc55fd8d285e4c950708ab6439e756e27bdb9
Hyeon Kim <uie10@naver.com>
2013-05-27 06:41:23 +0900

    최적화(2)
    screenstate, time <- static 멤버일 이유가 전혀 없음. 오히려 프로그램을 thread-unsafe하게 바꿈
    
    ON~iOFF <- 멤버일 이유가 없음. private static final로 변경

--- Client/src/com/goznauk/adoctor/ScreenStateReceiver.java
+++ Client/src/com/goznauk/adoctor/ScreenStateReceiver.java
@@ -7,34 +7,29 @@ import android.content.Intent;
 
 /**
  * Broadcast receiver로서, Screen on, off 될 때 Broadcast를 받아 DB에 시간과 함께 기록
+ * 
  * @author Choi H.John, Sky77
- *
+ * 
  */
 public class ScreenStateReceiver extends BroadcastReceiver {
 
-	public static boolean screenstate;
-	public static long time;
-
-	private final boolean ON = true;
-	private final boolean OFF = false;
-	private final String iON = Intent.ACTION_SCREEN_ON;
-	private final String iOFF = Intent.ACTION_SCREEN_OFF;
+	private static final boolean ON = true;
+	private static final boolean OFF = false;
+	private static final String iON = Intent.ACTION_SCREEN_ON;
+	private static final String iOFF = Intent.ACTION_SCREEN_OFF;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		time = System.currentTimeMillis();
-
 		DBAdapter adb = new DBAdapter(context, "scrlog");
 		adb.open();
 
-		if (intent.getAction().equals(iON)) screenstate = ON;
-		else if (intent.getAction().equals(iOFF)) screenstate = OFF;
-		
 		ContentValues cv = new ContentValues();
-		cv.put("time", time);
-		cv.put("screenstate", screenstate);
+		cv.put("time", System.currentTimeMillis());
+		if (intent.getAction().equals(iON)) cv.put("screenstate", ON);
+		else if (intent.getAction().equals(iOFF)) cv.put("screenstate", OFF);
+
 		adb.insertTable(cv);
-		
+
 		adb.close();
 	}
 }
