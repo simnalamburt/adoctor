d89320aed80fa8504a6f717ae440dd07d5135ccb
Choi H.John <goznauk@naver.com>
2013-06-11 18:51:45 +0900

    foreground 추가
    호환성 문제!!
    
    http://blog.naver.com/PostView.nhn?blogId=huewu&logNo=110119826822
    android.support.v4.jar 받아서 라이브러리 추가 후
    안될경우 Notification 을 NotificationCompat으로 바꿔서 하세요

--- Client/src/com/adoctor/adoctor/BRControlService.java
+++ Client/src/com/adoctor/adoctor/BRControlService.java
@@ -4,10 +4,13 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import android.app.AlarmManager;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
 import android.os.SystemClock;
 
@@ -19,13 +22,29 @@ import android.os.SystemClock;
 public class BRControlService extends Service {
 
 	ScreenStateReceiver mScreenStateReceiver = new ScreenStateReceiver();
+	Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.status_bar_icon);
 
 	/**
 	 * ScreenStateReceiver 등록
 	 */
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		super.onStartCommand(intent, flags, startId);
+		Notification notification = new Notification.Builder(getBaseContext())
+        .setContentTitle("ADoctor 실행중입니다. ")
+        .setContentText("통계 조사에 참여해 주셔서 감사합니다.")
+        .setSmallIcon(R.drawable.status_bar_icon)
+        .setLargeIcon(largeIcon)
+        .build();
+				
+		 
+	    Intent main = new Intent(this, MainActivity.class);
+	    main.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+	    //PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, main,  PendingIntent.FLAG_UPDATE_CURRENT);
+	
+	    //notification.setLatestEventInfo(this, "title", "text", pendingIntent);
+	    notification.flags |= Notification.FLAG_ONGOING_EVENT | Notification.FLAG_FOREGROUND_SERVICE | Notification.FLAG_NO_CLEAR;
+	 
+	    startForeground(889901, notification);
 
 		unregisterRestartAlarm(); 
 		IntentFilter filter = new IntentFilter();
