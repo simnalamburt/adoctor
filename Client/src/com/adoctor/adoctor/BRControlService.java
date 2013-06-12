1cbafc709c8ae3f8804805117a0c93b28ac2e7c4
Sky77 <sungx11551@naver.com>
2013-06-12 14:42:02 +0900

    로그 보내는 시간 조정 (2)
    서비스 시각 기준 가장 가까운 30분 or 정각에 맞춰짐

--- Client/src/com/adoctor/adoctor/BRControlService.java
+++ Client/src/com/adoctor/adoctor/BRControlService.java
@@ -56,10 +56,10 @@ public class BRControlService extends Service {
 		intent.setAction(ServiceRestarter.ACTION_RESTART_PERSISTENTSERVICE);
 		PendingIntent sender = PendingIntent.getBroadcast(
 				BRControlService.this, 0, intent, 0); // 브로드케스트할 Intent
-		long firstTime = SystemClock.elapsedRealtime();  // 현재 시간
-		firstTime += 1 * 1000; // 10초 후에 알람이벤트 발생
+		long start_time = SystemClock.elapsedRealtime();  // 현재 시간
+		start_time += 1 * 1000; // 10초 후에 알람이벤트 발생
 		AlarmManager am = (AlarmManager)getSystemService(ALARM_SERVICE); // 알람 서비스 등록
-		am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, firstTime, 10 * 1000, sender);
+		am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, start_time, 1 * 1000, sender);
 	}
 
 	void unregisterRestartAlarm() {
@@ -75,13 +75,18 @@ public class BRControlService extends Service {
 		Intent intent = new Intent(this, LogSender.class);
 		PendingIntent sender = PendingIntent.getBroadcast(this, 1, intent, 0);
 
-		// 내일 0시 0분에 처음 시작해서, 24시간 마다 실행되게
-		GregorianCalendar tomorrow = new GregorianCalendar();
-		tomorrow.add(Calendar.MINUTE, 30);
+		GregorianCalendar start_time = new GregorianCalendar();
+		start_time = new GregorianCalendar(
+				start_time.get(Calendar.YEAR),
+				start_time.get(Calendar.MONTH),
+				start_time.get(Calendar.DATE),
+				start_time.get(Calendar.HOUR_OF_DAY),
+				((start_time.get(Calendar.MINUTE)<30)?30:0));
+		if(start_time.get(Calendar.MINUTE)>30) start_time.add(Calendar.HOUR_OF_DAY, 1); 
 		AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
-		am.setInexactRepeating(AlarmManager.RTC_WAKEUP, tomorrow.getTimeInMillis(), AlarmManager.INTERVAL_HALF_HOUR, sender);
+		am.setInexactRepeating(AlarmManager.RTC_WAKEUP, start_time.getTimeInMillis(), AlarmManager.INTERVAL_HALF_HOUR, sender);
 	}
-	
+
 	public void unregisterSendAlarm() {
 		Intent intent = new Intent(this, LogSender.class);
 		PendingIntent sender = PendingIntent.getBroadcast(this, 1, intent, 0);
